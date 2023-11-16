5da3b372b86e3c7bd25f61842e71e8932345a1a0
Giu <giuliaghisolfi99@gmail.com>
2023-11-16 18:34:32 +0100

    PAL agent class

--- src/algorithms/PAL.py
+++ src/algorithms/PAL.py
@@ -1,25 +1,31 @@
+from src.agent import Agent
 from src.algorithms.utils import executing_policy
 #from spinningup.spinup.algos.tf1.trpo import trpo
 
-# PAL: Policy As Leader Algorithm
-def PAL(policy, model, env, n_episodes_per_iteration, alpha):
-    data_buffer = [] # list of tuples (state, action, reward, next_state)
 
-    # collect data executing policy in the environment
-    for _ in range(n_episodes_per_iteration):
-        episode = executing_policy(policy, env)
-        data_buffer.append(episode)
-    
-    # build policy-specific model
-    model = optimize_model(model, policy, data_buffer)
+class PAL(Agent):
+    def __init__(self, gamma, initial_state_coord, transition_matrix_initial_state):
+        super().__init__(gamma, initial_state_coord, transition_matrix_initial_state)
 
-    # improve policy (TRPO)
-    #policy = trpo(policy, model, alpha) #TODO: cambiare, vuole solo env con lo standard di OpenAI come input
-    trpo(env) #FIXME: non lo so ma dovrebbe essere una funzione che copia l'enviroment, poi deve ritornare la policy
+    # PAL: Policy As Leader Algorithm
+    def PAL(policy, model, env, n_episodes_per_iteration, alpha):
+        data_buffer = [] # list of tuples (state, action, reward, next_state)
 
-    return policy, model
+        # collect data executing policy in the environment
+        for _ in range(n_episodes_per_iteration):
+            episode = executing_policy(policy, env)
+            data_buffer.append(episode)
+        
+        # build policy-specific model
+        model = optimize_model(model, policy, data_buffer)
 
-def optimize_model(model, data_buffer):
-    # build model given data_buffer
-    # minimizzo KL div tra modello precedente e approx del modello ricavata dai dati
-    return model
+        # improve policy (TRPO)
+        #policy = trpo(policy, model, alpha) #TODO: cambiare, vuole solo env con lo standard di OpenAI come input
+        trpo(env) #FIXME: non lo so ma dovrebbe essere una funzione che copia l'enviroment, poi deve ritornare la policy
+
+        return policy, model
+
+    def optimize_model(model, data_buffer):
+        # build model given data_buffer
+        # minimizzo KL div tra modello precedente e approx del modello ricavata dai dati
+        return model
