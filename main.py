2fda5c5e285b0ee4eca24ee1712e744fff810b89
Giu <giuliaghisolfi99@gmail.com>
2023-12-02 00:41:33 +0100

    mc

--- main.py
+++ main.py
@@ -52,6 +52,6 @@ if __name__ == '__main__':
         beta=beta,
         render=render,
         render_wait=render_wait,
-        policy_path='src/saved_policy/PAL_policy_5_env.json', 
-        states_space_path='src/saved_policy/PAL_states_space_5_env.json'
+        policy_path='training_parameters/PAL/policy/policy_0_env.json', 
+        states_space_path='training_parameters/PAL/policy/states_space_0_env.json'
     )
